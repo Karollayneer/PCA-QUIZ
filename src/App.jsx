d17e1f757702bb9702f9d4c7061659e77c738f64
Karolayne Rodrigues <karollayneerod@gmail.com>
2023-04-30 17:56:12 -0300

    viet

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Sobre from './pags/Sobre'
-import Home from './pags/Home';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/sobre" component={Sobre} />
-      </Switch>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
-
-
+export default App
